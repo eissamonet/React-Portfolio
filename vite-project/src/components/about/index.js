2330c684d2fff8beda6990e4d0153cc0b39ce3f0
Eissa Purnell <eissamonet@gmail.com>
2023-10-25 21:20:35 -0500

    changes made to contact form

---
+++ vite-project/src/components/about/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import tailwind from 'tailwindcss';
+
+function about() {
+    return(
+    <section className="title">
+        <div className="row justify-content-center" id="about-container">
+        <col lg = "4" md = "6" sm = "12">
+        <center><img src{myImage} className= "my-image" alt="eissa-profile"/></center>
+        <p class="name">Eissa Purnell</p>
+        <p class="title">Full Stack Web Developer</p>
+        <h2 class="description">About Me</h2>
+            <p>Hi, I'm Eissa! I am a Full Stack Web Developer with a background in psychology and social work. I am passionate about creating applications that are accessible to all users. I am a team player, and I am always looking for opportunities to learn and grow as a developer.</p>
+        </col>
+        </div>
+    </section>
+    )}    
+
+    export default about;
---
+++ vite-project/src/components/contact/index.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+import {Row, Col} from 'react-bootstrap';
+
+function contactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            // conditional statement for other inputs
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        // console.log('errorMessage', errorMessage);
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        // console.log(formState);
+        if (errorMessage) {
+            setSuccessMessage('');
+        } else {
+            setSuccessMessage('Your message has been sent!');
+        }
+    }
+    return (
+        <section>
+            <h1 data-testid="h1tag">Contact me</h1>
+            <Row>
+                <Col>
+                    <form id="contact-form" onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="name">Name:</label>
+                            <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="email">Email address:</label>
+                            <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="message">Message:</label>
+                            <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+                        </div>
+                        {errorMessage && (
+                            <div>
+                                <p className="error-text">{errorMessage}</p>
+                            </div>
+                        )}
+                        {successMessage && (
+                            <div>
+                                <p className="success-text">{successMessage}</p>
+                            </div>
+                        )}
+                        <button data-testid="button" type="submit">Submit</button>
+                    </form>
+                </Col>
+            </Row>
+        </section>
+    );
+}
