168ab867a90eccf044dc09a82f709de98857fd1d
Eissa Purnell <eissamonet@gmail.com>
2023-10-26 22:36:35 -0500

    app.jsx added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,18 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import About from './components/about/index'
+import Contact from './components/contact/index'
+import Projects from './components/projects/index'
+import Resume from './components/resume/index'
+import Navigation from './components/navigation/index'
+import Footer from './components/footer/index'
+import Header from './components/header/index'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
         <About/>
-          
+        <Projects/>
+        <Resume/>
+        <Contact/>
+        <Footer/>
+        <Header/>
+        <Navigation/>          
         
     </>
-  )
+  );
 }
 
-export default App
+export default App;
