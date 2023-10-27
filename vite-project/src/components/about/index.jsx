7ad9b5090e317efabb38fbe47c9f8d132a40a2d7
Eissa Purnell <eissamonet@gmail.com>
2023-10-26 19:00:26 -0500

    links added to projects folder

--- vite-project/src/components/about/index.jsx
+++ vite-project/src/components/about/index.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import {Col} from 'react-bootstrap';
-import myImage from '../../assets/images/eissa-profile.jpg';
+import profilePicture from '../../assets/profilepicture.jpeg';
 
-function about() {
+function About() {
     return(
     <section className="title">
         <div className="row justify-content-center" id="about-container">
-        <col lg = "4" md = "6" sm = "12">
-        <center><img src{myImage} className= "my-image" alt="eissa-profile"/></center>
-        <p class="name">Eissa Purnell</p>
-        <p class="title">Full Stack Web Developer</p>
-        <h2 class="description">About Me</h2>
+        <Col lg = "4" md = "6" sm = "12">
+        <center><img src={profilePicture} className= "profile-picture" alt="eissa-profile"/></center>
+        <p className="name">Eissa Purnell</p>
+        <p className="title">Full Stack Web Developer</p>
+        <h2 className="description">About Me</h2>
             <p>Hi, I'm Eissa! I am a Full Stack Web Developer with a background in psychology and social work. I am passionate about creating applications that are accessible to all users. I am a team player, and I am always looking for opportunities to learn and grow as a developer.</p>
-        </col>
+        </Col>
         </div>
     </section>
     )}    
 
-    export default about;
+    export default About;
---
+++ vite-project/src/components/projects/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+// import 6 imgs of projects from src/assets
+
+const projects = () => {
+    const projects = [
+        {
+            id: 1,
+            src: workday 
+            repo: https://github.com/eissamonet/Work-Day-SchedulerLinks
+        },
+        {
+            id: 2,
+            src: readme 
+            repo: https://github.com/eissamonet/README-Generator
+        },
+        {
+            id: 3,
+            src: employeeTracker
+            repo:  https://github.com/eissamonet/EmployeeTrackerLinks to an external site.
+
+
+        },
+        {
+            id: 4,
+            src: ecommerce
+            repo:  https://github.com/eissamonet/E-Commerce
+        },
+        {
+            id: 5,
+            src:
+            repo: https://eissamonet.github.io/Horiseon/
+        },
+        {
+            id: 6,
+            src: textEditor
+            repo: https://github.com/eissamonet/TextEditor
+        },
+    ];
+
+    
+
